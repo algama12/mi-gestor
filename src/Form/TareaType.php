0b29d1cefc41cb26bbe94d238be62139f2cb29bb
algama12 <65444008+algama12@users.noreply.github.com>
2023-05-30 21:04:38 +0200

    añadiendo los choices de prioridad y margin en el boton eliminar

--- src/Form/TareaType.php
+++ src/Form/TareaType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Tarea;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,13 @@ class TareaType extends AbstractType
         $builder
             ->add('Descripcion')
             ->add('Horas')
-            ->add('Prioridad')
+            ->add('Prioridad', ChoiceType::class,[
+                'choices'=> [
+                    'Alta'=>'Alta',
+                    'Media'=> 'Media',
+                    'Baja'=>'Baja'
+                ]
+            ])
             ->add('Completado')
         ;
     }
