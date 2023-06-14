329fe628b80b935a1e6285249bd0cffd12b227e2
algama12 <65444008+algama12@users.noreply.github.com>
2023-06-14 17:52:01 +0200

    añadiendo el tema de bootstrap y además la gráfica, falta pulir el controlador para mostrar los datos reales

--- src/Controller/TareaController.php
+++ src/Controller/TareaController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 #[Route('/')]
@@ -83,6 +84,40 @@ class TareaController extends AbstractController
 
         return $this->redirectToRoute('app_tarea_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    #[Route('/cantidad-por-prioridad', name: 'app_cantidad_por_prioridad')]
+    public function cantidadPorPrioridadAction(EntityManagerInterface $entityManager): Response
+    {
+        $repository = $entityManager->getRepository(Elemento::class);
+
+        $alta = $repository->createQueryBuilder('e')
+            ->select('COUNT(e)')
+            ->where('e.prioridad = :alta')
+            ->setParameter('alta', 'alta')
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        $media = $repository->createQueryBuilder('e')
+            ->select('COUNT(e)')
+            ->where('e.prioridad = :media')
+            ->setParameter('media', 'media')
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        $baja = $repository->createQueryBuilder('e')
+            ->select('COUNT(e)')
+            ->where('e.prioridad = :baja')
+            ->setParameter('baja', 'baja')
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        return $this->render('index.html.twig', [
+            'alta' => $alta,
+            'media' => $media,
+            'baja' => $baja,
+        ]);
+    }
+    
 
 
 }
